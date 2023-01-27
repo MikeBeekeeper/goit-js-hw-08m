80fe89b165c0502dea7df54c5a05f8bd1d53542f
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-01-27 14:12:48 +0200

    final

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const galeryListOfImagesEl = document.querySelector('.gallery');
+const imageItemsMarkup = createImageMarkup(galleryItems);
+
+function createImageMarkup(items) {
+      return items.map( item =>
+      `
+      <a class="gallery__link" href="${item.original}" onclick="return false">
+        <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+        />
+</a>
+    `).join('')
+  }
+galeryListOfImagesEl.innerHTML = imageItemsMarkup;
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+     captionDelay: 250,
+     })
+
 // Change code below this line
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('.feedback-form input');
+const messageRef = document.querySelector('.feedback-form textarea');
+
+const formData = {};
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  console.log({
+    email: emailRef.value,
+    message: messageRef.value,
+  });
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+formRef.addEventListener('input', throttle(onFormData, 500));
+
+function onFormData(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (data) {
+    emailRef.value = data.email;
+    messageRef.value = data.message;
+  }
+}
+
+populateTextarea();
